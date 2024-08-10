c7c6f2ff07ae84f606b0e280bc35810e5f6299c7
Pedro <pedroaraujo4910@gmail.com>
2024-08-10 17:43:25 -0300

    Adicionando DB_HOST como variavél de ambiente

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(host='lab-render-pedroaraujo4910-1b67.e.aivencloud.com',
+    conn = psycopg2.connect(host=os.getenv("DB_HOST"),
                             database='movies',
                             user=os.getenv("DB_USERNAME"),
                             password=os.getenv("DB_PASSWORD"),
